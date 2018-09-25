14e2728122cd8adf60be55560c4e4bab7454ad60
zhf <732898328@qq.com>
2018-09-25 16:51:57 +0800

    add

---
+++ Java 8 in Action/code/src/main/java/lambdasinaction/chap10/Car.java
@@ -0,0 +1,14 @@
+package lambdasinaction.chap10;
+
+import java.util.*;
+
+public class Car
+{
+
+	private Optional<Insurance> insurance;
+
+	public Optional<Insurance> getInsurance()
+	{
+		return insurance;
+	}
+}
---
+++ Java 8 in Action/code/src/main/java/lambdasinaction/chap10/Insurance.java
@@ -0,0 +1,12 @@
+package lambdasinaction.chap10;
+
+public class Insurance
+{
+
+	private String name;
+
+	public String getName()
+	{
+		return name;
+	}
+}
