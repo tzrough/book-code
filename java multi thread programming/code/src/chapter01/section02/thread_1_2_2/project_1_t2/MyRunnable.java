6b689f07d0d07276a1a319daf132a40952a43e83
zhf <732898328@qq.com>
2018-11-01 17:43:52 +0800

    add

---
+++ java multi thread programming/code/src/chapter01/section02/thread_1_2_2/project_1_t2/MyRunnable.java
@@ -0,0 +1,11 @@
+package chapter01.section02.thread_1_2_2.project_1_t2;
+
+public class MyRunnable implements Runnable{
+	
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName());
+		System.out.println("н╦ллол!");
+	}
+	
+}
